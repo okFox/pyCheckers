fc2968f121379cfc13b161c592de1f08babaac6d
okFox <smayfield@gmail.com>
2020-09-12 22:33:22 -0700

    started event loop

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
+from checkers.constants import WIDTH, HEIGHT
 
+FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+# define event loop to run X times per second
+def main():
+  run = True
+  clock = pygame.time.Clock()
+
+  while run:
+    clock.tick(FPS)
+    
+    # check event list at each interval
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
