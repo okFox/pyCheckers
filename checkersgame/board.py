93abe1aeaf0f59d090d97237331889f0fab16ed3
okFox <smayfield@gmail.com>
2020-09-18 14:19:30 -0700

    sort of works using pyenv

--- checkersgame/board.py
+++ checkersgame/board.py
@@ -1,19 +1,22 @@
 import pygame
 
 # this is a relative import. ie. in the same package
-from checkersgame.constants import BLACK, ROWS, RED, SQUARE_SIZE
+from .constants import BLACK, ROWS, RED, SQUARE_SIZE
 
 
 class Board:
-  def __init__(self):
-    self.board = []
-    self.selected_piece = None
-    self.red_left = self.white_left = 12  # 12 pieces/player to be decremented
-    self.red_kings = self.white_kings = 0
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        # 12 pieces/player to be decremented
+        self.red_kings = self.white_kings = 0
+        self.red_left = self.white_left = 12
 
 # win stands for window
-  def draw_squares(self, win):
-    win.fill(BLACK)
-    for row in range(ROWS):
-      for col in range(row % 2, ROWS): # the modulo allows for starting red on every other row
-        pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)) # (left, top, width, height)
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            # the modulo allows for starting red on e/o row
+            for col in range(row % 2, ROWS):  
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))  
+                # (left, top, width, height)
