36b931bb780828b6c8bd136768416301964418b7
okFox <smayfield@gmail.com>
2020-09-18 17:42:36 -0700

    fixing linter errors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 from checkersgame.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkersgame.board import Board
 
@@ -6,25 +7,26 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
+
 # define event loop to run X times per second
 def main():
-  run = True
-  clock = pygame.time.Clock()
-  board = Board()
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
 
-  while run:
-    clock.tick(FPS)
-    
+    while run:
+        clock.tick(FPS)
     # check event list at each interval
     for event in pygame.event.get():
-      if event.type == pygame.QUIT:  # should quit be capitalized??
-        run = False
+        if event.type == pygame.QUIT:  # should quit be capitalized??
+            run = False
 
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pass
     board.draw_squares(WIN)
     pygame.display.update()
 
-  pygame.quit()
+    pygame.quit()
+
 
 main()
